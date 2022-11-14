176c3f0ec43c6c9e800c1bc9ce34efe6db0ca990
AlbertoLM98 <albertolm13@gmail.com>
2022-11-14 12:15:38 +0100

    Subida repositorio del proyecto.

---
+++ NttDevopsBootcamps/src/HolaMundo.java
@@ -0,0 +1,12 @@
+
+public class HolaMundo {
+	/**
+	 * Esto es una prueba de Git.
+	 * @param args
+	 */
+
+	public static void main (String[]args) {
+		System.out.println("Esto es una prueba de Git");
+	}
+
+}
